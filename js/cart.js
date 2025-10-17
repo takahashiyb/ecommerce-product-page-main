44615d659ff47654224be65574a59364304f8e3b
takahashiyb <takahashiyb89@gmail.com>
2025-10-18 04:41:26 +0800

    Create Nav bar and assign open and close functions for the nav bar.

--- js/cart.js
+++ js/cart.js
@@ -7,3 +7,99 @@ export const cartList = [
     quantity: 3,
   },
 ];
+
+export function giveCartButtonFunction() {
+  const elementCart = document.getElementById("cart-link");
+
+  elementCart.addEventListener("click", () => {
+    if (elementCart.getAttribute("aria-expanded") === "false") {
+      openCheckoutPopup();
+    } else {
+      closeCheckoutPopup();
+    }
+  });
+}
+
+function openCheckoutPopup() {
+  const elementBody = document.querySelector("body");
+
+  const elementCart = document.getElementById("cart-link");
+
+  elementCart.setAttribute("aria-expanded", "true");
+
+  const elementPopup = document.createElement("div");
+  elementPopup.classList.add("popup-cart");
+  elementPopup.setAttribute("role", "region");
+
+  const elementMain = elementBody.querySelector("main");
+
+  // Cart Title
+  const cartHeader = document.createElement("p");
+  cartHeader.classList.add("popup-cart-title");
+  cartHeader.innerHTML = `Cart`;
+  elementPopup.append(cartHeader);
+
+  const cartListWrapper = document.createElement("div");
+  cartListWrapper.classList.add("cart-item-list-wrapper");
+
+  // When empty
+  if (cartList.length === 0) {
+    const cartEmptyWrapper = document.createElement("p");
+    cartEmptyWrapper.classList.add("popup-cart-empty-text");
+    cartEmptyWrapper.innerHTML = "Your cart is empty.";
+
+    cartListWrapper.classList.add("popup-cart-empty");
+
+    cartListWrapper.append(cartEmptyWrapper);
+  }
+
+  if (cartList.length !== 0) {
+    for (let i = 0; i < cartList.length; i++) {
+      const cartItem = cartList[i];
+
+      const elementCartItem = document.createElement("div");
+      elementCartItem.id = cartItem["serial"];
+      elementCartItem.classList.add("cart-item-wrapper");
+      elementCartItem.innerHTML = `
+          <img
+            class="cart-item-image"
+            src="${cartItem["image src"]}"
+            alt="product image"
+          />
+          <div>
+            <p>${cartItem["name"]}</p>
+            <p>$${cartItem["actual price"].toFixed(2)} x ${
+        cartItem["quantity"]
+      } <strong>$${(cartItem["actual price"] * cartItem["quantity"]).toFixed(
+        2
+      )}</strong></p>
+          </div>
+          <button aria-label="removes item from the cart">
+            <img src="./images/icon-delete.svg" alt="delete icon" />
+          </button>
+      `;
+      cartListWrapper.append(elementCartItem);
+    }
+
+    const elementLinkCheckout = document.createElement("a");
+    elementLinkCheckout.classList.add("popup-link-checkout");
+    elementLinkCheckout.innerHTML = "Checkout";
+    elementLinkCheckout.href = "#";
+    elementLinkCheckout.setAttribute("aria-label", "leads to checkout page");
+
+    cartListWrapper.append(elementLinkCheckout);
+  }
+
+  elementPopup.append(cartListWrapper);
+
+  elementBody.insertBefore(elementPopup, elementMain);
+}
+
+function closeCheckoutPopup() {
+  const elementCart = document.getElementById("cart-link");
+  const elementPopup = document.querySelector(".popup-cart");
+  elementPopup.remove();
+  elementCart.setAttribute("aria-expanded", "false");
+}
+
+giveCartButtonFunction();
--- js/index.js
+++ js/index.js
@@ -1,87 +1,95 @@
-import { cartList } from "./cart.js";
+import { giveCartButtonFunction } from "./cart.js";
 
-const elementCart = document.getElementById("cart-link");
+function giveCloseNavButtonFunction(button) {
+  button.addEventListener("click", () => {
+    const regionNav = document.querySelector(".navigation-bar");
+    const buttonNavMenu = document.querySelector(".navigation-menu-button");
 
-elementCart.addEventListener("click", () => {
-  if (elementCart.getAttribute("aria-expanded") === "false") {
-    openCheckoutPopup();
-  } else {
-    const elementBody = document.querySelector("body");
-    const elementPopup = document.querySelector(".popup-cart");
-    elementBody.removeChild(elementPopup);
-    elementCart.setAttribute("aria-expanded", "false");
-  }
-});
+    regionNav.remove();
+    buttonNavMenu.setAttribute("aria-expanded", false);
 
-function openCheckoutPopup() {
-  const elementBody = document.querySelector("body");
+    const pageHeader = document.querySelector("header");
+    const pageMain = document.querySelector("main");
+    const pageFooter = document.querySelector("footer");
 
-  elementCart.setAttribute("aria-expanded", "true");
-
-  const elementPopup = document.createElement("div");
-  elementPopup.classList.add("popup-cart");
-  elementPopup.setAttribute("role", "region");
-
-  const elementMain = elementBody.querySelector("main");
-
-  // Cart Title
-  const cartHeader = document.createElement("p");
-  cartHeader.classList.add("popup-cart-title");
-  cartHeader.innerHTML = `Cart`;
-  elementPopup.append(cartHeader);
-
-  const cartListWrapper = document.createElement("div");
-  cartListWrapper.classList.add("cart-item-list-wrapper");
-
-  // When empty
-  if (cartList.length === 0) {
-    const cartEmptyWrapper = document.createElement("p");
-    cartEmptyWrapper.classList.add("popup-cart-empty-text");
-    cartEmptyWrapper.innerHTML = "Your cart is empty.";
-
-    cartListWrapper.classList.add("popup-cart-empty");
-
-    cartListWrapper.append(cartEmptyWrapper);
-  }
-
-  if (cartList.length !== 0) {
-    for (let i = 0; i < cartList.length; i++) {
-      const cartItem = cartList[i];
-
-      const elementCartItem = document.createElement("div");
-      elementCartItem.id = cartItem["serial"];
-      elementCartItem.classList.add("cart-item-wrapper");
-      elementCartItem.innerHTML = `
-          <img
-            class="cart-item-image"
-            src="${cartItem["image src"]}"
-            alt="product image"
-          />
-          <div>
-            <p>${cartItem["name"]}</p>
-            <p>$${cartItem["actual price"].toFixed(2)} x ${
-        cartItem["quantity"]
-      } <strong>$${(cartItem["actual price"] * cartItem["quantity"]).toFixed(
-        2
-      )}</strong></p>
-          </div>
-          <button aria-label="removes item from the cart">
-            <img src="./images/icon-delete.svg" alt="delete icon" />
-          </button>
-      `;
-      cartListWrapper.append(elementCartItem);
-    }
-
-    const elementLinkCheckout = document.createElement("a");
-    elementLinkCheckout.classList.add("popup-link-checkout");
-    elementLinkCheckout.innerHTML = "Checkout";
-    elementLinkCheckout.href = "#";
-    elementLinkCheckout.setAttribute("aria-label", "leads to checkout page");
-
-    cartListWrapper.append(elementLinkCheckout);
-  }
-
-  elementPopup.append(cartListWrapper);
-
-  elementBody.insertBefore(elementPopup, elementMain);
+    pageHeader.inert = false;
+    pageMain.inert = false;
+    pageFooter.inert = false;
+  });
 }
+
+// giveCloseNavButtonFunction();
+
+function giveNavButtonFunction() {
+  const elementBody = document.querySelector("body");
+  const pageHeader = elementBody.querySelector("header");
+
+  const buttonNavMenu = elementBody.querySelector(".navigation-menu-button");
+  buttonNavMenu.addEventListener("click", () => {
+    const elementNav = document.createElement("nav");
+    elementNav.classList.add("navigation-bar");
+
+    const divLightboxUnfocus = document.createElement("div");
+    divLightboxUnfocus.classList.add("light-box-unfocus");
+
+    const divLightboxFocus = document.createElement("div");
+    divLightboxFocus.classList.add("light-box-focus");
+
+    const buttonCloseNav = document.createElement("div");
+    buttonCloseNav.classList.add("close-nav-button");
+    buttonCloseNav.innerHTML = '<img src="./images/icon-close.svg" alt="" />';
+    giveCloseNavButtonFunction(buttonCloseNav);
+
+    const listNav = document.createElement("ul");
+    listNav.innerHTML = `
+      <li>
+        <a href="#" aria-label="click to see collections page"
+          >Collections</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          aria-label="redirects to page focused on men's products"
+          >Men</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          aria-label="redirects to page focused on women's products"
+          >Women</a
+        >
+      </li>
+      <li>
+        <a href="#" aria-label="redirects to company information page"
+          >About</a
+        >
+      </li>
+      <li>
+        <a href="#" aria-label="redirects to site contact information"
+          >Contact</a
+        >
+      </li>    
+    `;
+
+    elementNav.appendChild(divLightboxUnfocus);
+    divLightboxUnfocus.appendChild(divLightboxFocus);
+    divLightboxFocus.appendChild(buttonCloseNav);
+    divLightboxFocus.appendChild(listNav);
+
+    elementBody.insertBefore(elementNav, pageHeader);
+
+    const buttonNavMenu = elementBody.querySelector(".navigation-menu-button");
+    buttonNavMenu.setAttribute("aria-expanded", "true");
+
+    const pageMain = document.querySelector("main");
+    const pageFooter = document.querySelector("footer");
+
+    pageHeader.inert = true;
+    pageMain.inert = true;
+    pageFooter.inert = true;
+  });
+}
+
+giveNavButtonFunction();
